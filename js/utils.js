0721c958103910d6e553da86d3d4e538365e0cea
Richard Rosenberg <rosenbr73@gmail.com>
2021-04-07 16:13:55 -0700

    Eat/Sleep/Play Buttons working

--- js/utils.js
+++ js/utils.js
@@ -6,76 +6,46 @@ const pachy = {
     energy: 0,  // -1/30s  10=dead, 0=happy
     boredom: 0,  // +1/30s 0=happy, 10=run away (Ice Box, reverse it)
 };
-
-// <!-- $(`#start`).on("click", ageIncrease); -->
-                // or
-// $(`#start`).on("click", function(){
-    // $(".name").text($(`#input-name`).val());
-// });
-
-
 $(`#munch`).on("click", feedPachy);
 function feedPachy(){
     pachy.hunger = pachy.hunger -1.5;
     pachy.energy = pachy.energy -0.25;
     console.log("noms");
 };
-// sleep
-$(`#nap`).on("click", playPachy);
-function playPachy(){
+$(`#nap`).on("click", sleepPachy);
+function sleepPachy(){
     pachy.enerygy = pachy.energy -1;
-    pachy.boredom = pachy.boredom -0.25;
+    pachy.boredom = pachy.boredom +0.25;
     console.log("zzzz");
 };
-
-// play
-/* $(`#munch`).on("click", feedPachy);
-function feedPachy(){
-    pachy.hunger = pachy.hunger -1.5;
-    pachy.energy = pachy.energy -0.25;
+$(`#rumpus`).on("click", playPachy);
+function playPachy(){
+    pachy.boredom = pachy.boredom -1.5;
+    pachy.energy = pachy.energy +0.25;
     console.log("noms");
-}; */
-
-
-// munch(){
-    //button, onClick -> pet eats, adjusts Hunger
-    //+1.5 Hunger, +0.25 Energy
-
-// };
-
-
-// nap(){
-            // button, onClick -> pet sleeps, adjusts Energy
-            // +1.5 energy, +.025 boredom
-        // },
-        
-// rumpus(){
-            // button, onClick -> pet plays, adjusts Boredom
-            // -1.5 bordeom, -0.25 energy
-        // },
-    // },
-function ageIncrease(){   //age
+};
+function ageIncrease(){   
     pachy.age = setInterval(increaseAge, 3000);
     console.log("age up");
 };
 function increaseAge(){
     pachy.age++;
 };
-function hungerIncrease(){  //hunger
+function hungerIncrease(){  
     pachy.hunger = setInterval(increaseHunger, 20000);
     console.log("hunger up");
 };
 function increaseHunger(){
     pachy.hunger++;
 };
-function energyIncrease(){  //energy
+function energyIncrease(){  
     pachy.energy = setInterval(increaseEnergy, 25000);
     console.log("energy up");
 };
 function increaseEnergy(){  
     pachy.energy++;
 };
-function boredomIncrease(){  //boredom
+function boredomIncrease(){  
     pachy.boredom = setInterval(increaseBoredom, 15000);
     console.log("boredom up");
 };
@@ -98,12 +68,12 @@ function increaseBoredom(){
             //funciton to increase age w/time linked to timer √
         // connect timers to start button √
     // adjusting HEB in gameplay
-        // +hunger
-            //onCLick
-        // +energy
-            //onClick
-        // +boredom
-            //onClick
+        // +hunger √
+            //onCLick √
+        // +energy √
+            //onClick √
+        // +boredom √
+            //onClick √
     //HEB counters
     
     // game over
