3bd99e04420c38d784f4fef687272c6c51aeb451
Richard Rosenberg <rosenbr73@gmail.com>
2021-04-07 16:56:07 -0700

    EOD Commit 4.7

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,11 @@
+// TODO add age on Start click
+// add counters
+// game over
+// Clean up user stories/README
+// make it pretty
+    //add animations
+
+
 // stub √
 const pachy = {
     age: 0,
@@ -6,24 +14,32 @@ const pachy = {
     energy: 0,  // -1/30s  10=dead, 0=happy
     boredom: 0,  // +1/30s 0=happy, 10=run away (Ice Box, reverse it)
 };
+
+
 $(`#munch`).on("click", feedPachy);
 function feedPachy(){
     pachy.hunger = pachy.hunger -1.5;
     pachy.energy = pachy.energy -0.25;
     console.log("noms");
 };
+
+
 $(`#nap`).on("click", sleepPachy);
 function sleepPachy(){
-    pachy.enerygy = pachy.energy -1;
+    pachy.energy = pachy.energy -1;
     pachy.boredom = pachy.boredom +0.25;
     console.log("zzzz");
 };
+
+
 $(`#rumpus`).on("click", playPachy);
 function playPachy(){
     pachy.boredom = pachy.boredom -1.5;
     pachy.energy = pachy.energy +0.25;
-    console.log("noms");
+    console.log("weeee!");
 };
+
+
 function ageIncrease(){   
     pachy.age = setInterval(increaseAge, 3000);
     console.log("age up");
@@ -31,6 +47,8 @@ function ageIncrease(){
 function increaseAge(){
     pachy.age++;
 };
+
+
 function hungerIncrease(){  
     pachy.hunger = setInterval(increaseHunger, 20000);
     console.log("hunger up");
@@ -38,6 +56,8 @@ function hungerIncrease(){
 function increaseHunger(){
     pachy.hunger++;
 };
+
+
 function energyIncrease(){  
     pachy.energy = setInterval(increaseEnergy, 25000);
     console.log("energy up");
@@ -45,6 +65,8 @@ function energyIncrease(){
 function increaseEnergy(){  
     pachy.energy++;
 };
+
+
 function boredomIncrease(){  
     pachy.boredom = setInterval(increaseBoredom, 15000);
     console.log("boredom up");
@@ -75,14 +97,13 @@ function increaseBoredom(){
         // +boredom √
             //onClick √
     //HEB counters
-    
+        //update w/timer
     // game over
     
     
 $(`#start`).on("click", function(){
     $(".name").text($(`#input-name`).val());
 });
-
 $(`#start`).on("click", ageIncrease);
 $(`#start`).on("click", hungerIncrease);
 $(`#start`).on("click", energyIncrease);
