f56f7d7526e4be6879950f94406b3090ef104ee5
Richard Rosenberg <rosenbr73@gmail.com>
2021-04-07 10:29:27 -0700

    Name Input Working

--- js/utils.js
+++ js/utils.js
@@ -31,55 +31,76 @@ console.log("testing");
 
 // create event for game end
 
+const pachy = {
 
-const $pet = {
-    age: 0,
-    hunger: 0,  // -1/30s
-    energy: 0,  // -1/30s
-    boredom: 0,  // +1/30s
-    
-    
-    
-};
+    age: 0,    // +1/60s max 10
+    name:"",
+    hunger: 10,  // -1/30s  0=dead, 10=happy
+    energy: 10,  // -1/30s  0=dead, 10=happy
+    boredom: 0,  // +1/30s 0=happy, 10=run away
 
-const game = {
-    munch(){
+    pet () {        
+        $name: $(`#input-name`).val();
+        pachy.name = $name;
+        
+        
+        //buttons
+        //click listener
+        //adjust values
+    },
+    // $game  = {
+        
+        
         // button, onClick -> feeds pet, adjusts Hunger
         // +1.5 hunger, +.025 energy
-    },  
-    
-    nap(){
-        // button, onClick -> pet sleeps, adjusts Energy
-        // +1.5 energy, +.025 boredom
-    },
-    
-    rumpus(){
-        // button, onClick -> pet plays, adjusts Boredom
-        // -1.5 bordeom, -0.25 energy
-    },
+        // munch(){
+            
+        // },
+        
+        // nap(){
+            // button, onClick -> pet sleeps, adjusts Energy
+            // +1.5 energy, +.025 boredom
+        // },
+        
+        // rumpus(){
+            // button, onClick -> pet plays, adjusts Boredom
+            // -1.5 bordeom, -0.25 energy
+        // },
+    // },
 };
-
-// Click Start to start a new game  
+    
+    // Click Start to start a new game  
     // click start √ 
     //have user input name √, 
     //store name
-        //create variabble for name
-        //input value of prompt in name variable
-        
-        
-        //update div 
+    //create variabble for name
+    //input value of prompt in name variable
+    
+    
+    //update div 
     //add HEB values to pet
     //start timers
-
-    // let $name = $(`<span class"name">$name</span>`).appendTo(`<div>`);
-    $(`#start`).click(function() {
-        const value = document.querySelector("input").value;
-        
-        // let $name = $petName.text(val($petName));
-        console.log("click test");
-    });
-
-/* 
+    /*
+$("#start").click(function() {
+    let $name = $(`<span class="name">Name: ${$name}</span>`);
+    $(`.white-space`).append($name)
+    function showName(){
+        let $name = $("#input-name").text($name.val());
+    };
+    console.log("click test");
+});*/
+    
+   
+$(`#start`).on("click", function(){
+    // let $name = $(`<span class="name">Name: ${$name}</span>`);
+    $(".name").text($('#input-name').val());
+    console.log("name");
+});
+   /*let bla = $('#inout-name').val();
+   $('#input-name').val(bla);
+   */
+   
+   /* 
 $("button").click(function(){
   $("input:text").val("Glenn Quagmire");
 });
